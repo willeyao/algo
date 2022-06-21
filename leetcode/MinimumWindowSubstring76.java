5ddfd6c429687b311dfa14188944eb61220a9417
willeyao <willeyao@163.com>
2022-06-21 10:22:01 +0000

    array instead of map

--- leetcode/MinimumWindowSubstring76.java
+++ leetcode/MinimumWindowSubstring76.java
@@ -7,7 +7,7 @@ public class MinimumWindowSubstring76 {
 
     /**
      * 性能优化
-     * 主要调整了计算两个字符串相似度的方法，通过加法计数法来计算
+     * key数量有限时，可以用数字代替map，数组下标为key
      * @param s
      * @param t
      * @return
@@ -16,36 +16,24 @@ public class MinimumWindowSubstring76 {
         if (s.length() < t.length()) {
             return "";
         }
-        Map<Character, Integer> charMap = new HashMap<>();
+        int sLen = s.length();
+        int tLen = t.length();
+        int[] charMap = new int[128];
         for (char ch : t.toCharArray()) {
-            int value = charMap.getOrDefault(ch, 0);
-            charMap.put(ch, value+1);
+            charMap[ch]--;
         }
-        Map<Character, Integer> sMap = new HashMap<>();
         int left = 0;
         int count = 0;
         String rt = "";
-        for (int i=0;i<s.length();i++) {
-            char ch = s.charAt(i);
-            if (!charMap.containsKey(ch)) {
-                continue;
-            }
-            sMap.put(ch, sMap.getOrDefault(ch, 0)+1);
-            if (sMap.get(ch) <= charMap.get(ch)) {
+        for (int i=0;i<sLen;i++) {
+            charMap[s.charAt(i)]++;
+            if (charMap[s.charAt(i)] <= 0) {
                 count++;
             }
-            
-            if (count == t.length()) {
-                while (left <= i) {
-                    char tmpChar = s.charAt(left);
-                    if (!charMap.containsKey(tmpChar) || sMap.get(tmpChar) > charMap.get(tmpChar)) {
-                        left++;
-                        if (sMap.containsKey(tmpChar)) {
-                            sMap.put(tmpChar, sMap.get(tmpChar) -1);
-                        }
-                    } else {
-                        break;
-                    }
+            if (count == tLen) {
+                while (left <= i && charMap[s.charAt(left)] > 0) {
+                    charMap[s.charAt(left)]--;
+                    left++; 
                 }
                 if (rt == "" || rt.length() > i - left + 1) {
                     rt = s.substring(left, i+1);
